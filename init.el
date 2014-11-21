c185a4720ed6e227f6a479f9295b984fa5519ce8
Yu Nejigane <yu.nejigane@gmail.com>
2014-11-21 23:32:35 +0900

    Add setting for trailing white space and newline

--- init.el
+++ init.el
@@ -1,11 +1,14 @@
 (setq default-input-method "MacOSX")
-
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 
 (setq make-backup-files nil)
+
 (setq-default tab-width 4 indent-tabs-mode nil)
 (setq js-indent-level 2)
+
+(setq-default show-trailing-whitespace t)
+(setq require-final-newline t)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
